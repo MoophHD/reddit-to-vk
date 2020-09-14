39f6d3e677a26e4948b89bc7dde0950557ef0bf0
moophhd <d_sizykh@mail.ru>
2020-09-14 23:42:45 +0300

    getting img url

--- src/reddit.js
+++ src/reddit.js
@@ -1,6 +1,31 @@
 const pupeteer = require("puppeteer");
 
-const getRedditUrl = (subreddit) => `https://old.reddit.com/r/${subreddit}/top?sort=top&t=all`;
+const getRedditUrl = (subreddit) =>
+  // `https://old.reddit.com/r/${subreddit}/`;
+  `https://old.reddit.com/r/${subreddit}/top?sort=top&t=all`;
+
+const getImgUrl = async (img) => {
+  try {
+    let imgUrl = "";
+
+    if (img != null) {
+      const propertyHandle = await img.getProperty("src");
+      imgUrl = await propertyHandle.jsonValue();
+
+      imgUrl = imgUrl.replace("preview", "i");
+    }
+
+    //trim sizing params
+    if (imgUrl.indexOf("?") !== -1)
+      imgUrl = imgUrl.slice(0, imgUrl.indexOf("?"));
+
+    return imgUrl;
+  } catch (e) {
+    console.log(`getImg error`);
+    console.log(e);
+    return "";
+  }
+};
 
 const self = {
   browser: null,
@@ -16,16 +41,38 @@ const self = {
   },
 
   getResult: async (postsCount) => {
-    const elements = await self.page.$$('#siteTable > div[class*="thing"]');
+    try {
+      const elements = await self.page.$$('#siteTable > div[class*="thing"]');
 
-    for (let element of elements) {
-      const title = await element.$eval(('p[class="title"]'), node => node.innerText.trim());
-      const score = await element.$eval(('div[class="score likes"]'), node => node.innerText.trim());
+      for (let element of elements) {
+        // Expand post, to get img
+        const expandBtn = await element.$('div[class*="expando-button"]');
+        console.log(expandBtn);
+        if (expandBtn != null) expandBtn.click();
 
-      console.log(`title ${title}`);
-      console.log(`score ${score}`);
+        const title = await element.$eval('p[class="title"] > a', (node) =>
+          node.innerText.trim()
+        );
+        const score = await element.$eval('div[class="score likes"]', (node) =>
+          node.innerText.trim()
+        );
+
+        const img = await element.$('img[class="preview"]');
+
+        if (expandBtn) expandBtn.click();
+        console.log(img);
+
+        let imgUrl = await getImgUrl(img);
+
+        console.log(`title: ${title}`);
+        console.log(`score: ${score}`);
+        console.log(`imgUrl: ${imgUrl}`);
+      }
+    } catch (e) {
+      console.log(`getResult error`);
+      console.log(e);
     }
-  }
+  },
 };
 
 module.exports = self;
