c3a6a401cba3788b940e47558ec21b2d36acd51a
moophhd <d_sizykh@mail.ru>
2020-09-15 17:09:37 +0300

    gettings img urls: fixes for cases when expand block is missing & when expand content is a video

--- src/reddit.js
+++ src/reddit.js
@@ -1,31 +1,19 @@
 const pupeteer = require("puppeteer");
 
-const getRedditUrl = (subreddit) =>
-  // `https://old.reddit.com/r/${subreddit}/`;
-  `https://old.reddit.com/r/${subreddit}/top?sort=top&t=all`;
+const getImgUrl = (cachedHtml) => {
+  let url = "";
 
-const getImgUrl = async (img) => {
-  try {
-    let imgUrl = "";
-
-    if (img != null) {
-      const propertyHandle = await img.getProperty("src");
-      imgUrl = await propertyHandle.jsonValue();
-
-      imgUrl = imgUrl.replace("preview", "i");
-    }
-
-    //trim sizing params
-    if (imgUrl.indexOf("?") !== -1)
-      imgUrl = imgUrl.slice(0, imgUrl.indexOf("?"));
-
-    return imgUrl;
-  } catch (e) {
-    console.log(`getImg error`);
-    console.log(e);
-    return "";
+  const regexMatch = cachedHtml.match(/(?<=src=")([^"]+)(\.jpg|\.png|\.jpeg|\.gif)/);
+  if (regexMatch) {
+    url = regexMatch[0];
+    url = url.replace("preview", "i");
   }
+
+  return url;
 };
+
+const getRedditUrl = (subreddit) =>
+  `https://old.reddit.com/r/${subreddit}/top?sort=top&t=all`;
 
 const self = {
   browser: null,
@@ -45,24 +33,23 @@ const self = {
       const elements = await self.page.$$('#siteTable > div[class*="thing"]');
 
       for (let element of elements) {
-        // Expand post, to get img
-        const expandBtn = await element.$('div[class*="expando-button"]');
-        console.log(expandBtn);
-        if (expandBtn != null) expandBtn.click();
-
-        const title = await element.$eval('p[class="title"] > a', (node) =>
+        const title = await element.$eval('p.title > a', (node) =>
           node.innerText.trim()
         );
-        const score = await element.$eval('div[class="score likes"]', (node) =>
+        const score = await element.$eval('div.score.likes', (node) =>
           node.innerText.trim()
         );
 
-        const img = await element.$('img[class="preview"]');
 
-        if (expandBtn) expandBtn.click();
-        console.log(img);
-
-        let imgUrl = await getImgUrl(img);
+        let cachedHtml = '';
+        if (await element.$('div.expando.expando-uninitialized') !== null) {
+          cachedHtml = await element.$eval(
+            'div.expando.expando-uninitialized',
+            (el) => el.getAttribute("data-cachedhtml")
+          );
+        }
+   
+        const imgUrl = getImgUrl(cachedHtml);
 
         console.log(`title: ${title}`);
         console.log(`score: ${score}`);
